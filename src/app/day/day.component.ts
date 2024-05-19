f36cab4ab38f52054e2614c130c89f186d12cd7b
David Thissen <dthissen@udel.edu>
2024-05-19 12:50:31 -0400

    doing some docstrings

--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -3,10 +3,19 @@ import html from "./day.component.html";
 import css from "./day.component.css";
 import { HabitCompletionComponent } from "../habit-completion/habit-completion.component";
 import { HabitComponent } from "../habit/habit.component";
-
+/**
+ * DayComponent represents a visual component for displaying a day's habits and their completion status.
+ * @extends EzComponent
+ */
 export class DayComponent extends EzComponent {
     private date: string;
     private todaysHabits: HabitCompletionComponent[] = [];
+
+    /**
+     * Creates an instance of DayComponent.
+     * @param date - The date for this component.
+     * @param habits - An array of HabitComponent instances for this day.
+     */
 
     constructor(date: string, habits: HabitComponent[]) {
         super(html, css);
@@ -21,6 +30,10 @@ export class DayComponent extends EzComponent {
         }
     }
 
+    /**
+     * Adds a habit to today's habits if it doesn't already exist.
+     * @param habit - The HabitCompletionComponent to add.
+     */
     addHabit(habit: HabitCompletionComponent) {
         let checker: boolean = true;
 
@@ -38,11 +51,18 @@ export class DayComponent extends EzComponent {
             this.todaysHabits.push(tempHabitCompletionItem);
         }
     }
+    /**
+     * Retrieves the habits for today.
+     * @returns An array of HabitCompletionComponent instances.
+     */
 
     getTodaysHabits() {
         return this.todaysHabits;
     }
-
+    /**
+     * Retrieves the date for this component.
+     * @returns The date as a string.
+     */
     getDate() {
         return this.date;
     }
--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -7,6 +7,10 @@ import {
 import html from "./graph.component.html";
 import css from "./graph.component.css";
 
+/**
+ * GraphComponent represents a visual component for displaying a habit's progress.
+ * @extends EzComponent
+ */
 export class GraphComponent extends EzComponent {
     @BindStyleToNumberAppendPx("bar", "width")
     private width: number = 0;
@@ -22,25 +26,43 @@ export class GraphComponent extends EzComponent {
         this.habit = habit;
     }
 
+    /**
+     * Updates the width of the progress bar based on timesCompleted.
+     */
+
     increaseWidth() {
         this.width = this.timesCompleted * 10;
     }
-
+    /**
+     * Retrieves the title of the graph.
+     * @returns The name of the habit.
+     */
     getGraphTitle() {
         return this.habit;
     }
-
+    /**
+     * Retrieves the number of times the habit has been completed.
+     * @returns The number of times completed.
+     */
     getTimesCompleted() {
         return this.timesCompleted;
     }
-
+    /**
+     * Sets the number of times the habit has been completed.
+     * @param times - The number of times to set.
+     */
     setTimesCompleted(times: number) {
         this.timesCompleted = times;
     }
-
+    /**
+     * Increases the number of times the habit has been completed by one.
+     */
     addCompletion() {
         this.timesCompleted++;
     }
+    /**
+     * Decreases the number of times the habit has been completed by one.
+     */
     removeCompletion() {
         this.timesCompleted--;
     }
